b03a52563251080f823ee9819b2285cf338bf2e1
Haiming <111258295+Hsumi0ng@users.noreply.github.com>
2023-03-03 19:40:59 -0600

    Update test_RAM.cpp

--- lab6/test_RAM.cpp
+++ lab6/test_RAM.cpp
@@ -11,10 +11,10 @@ class Pkt_constraint : virtual public scv_constraint_base
 
 	SCV_CONSTRAINT_CTOR(Pkt_constraint) {
 		// define constraints
-		// ...
-		// ...
-		// ...
-	}
+		SCV_CONSTRAINT( sAddr() < (1<<ADDR_WIDTH));
+
+		SCV_CONSTRAINT( sInData() < (1<<DATA_WIDTH));
+		}
 };
 
 int sc_main (int argc, char* argv[]) {
@@ -29,7 +29,7 @@ int sc_main (int argc, char* argv[]) {
 	int i = 0;
 	
 	// Connect the DUT(Design Under Test)
-	RAM RAM_01("SIMULATION_RAM");
+	SRAM RAM_01("SIMULATION_RAM");
 		RAM_01.InData(tInData);
         	RAM_01.Addr(tAddr);
 		RAM_01.bCE(tbCE);
@@ -51,26 +51,39 @@ int sc_main (int argc, char* argv[]) {
 	tbWE.write(1);
 	sc_start(5);
 
-	Pkt_constraint cPkt("Pkt_Test");	
+	Pkt_constraint cPkt("Pkt_Test");
+
+	typedef pair < sc_uint<ADDR_WIDTH>,sc_uint<ADDR_WIDTH> > addr_range;
+	scv_bag<addr_range> addr_list;
+	addr_list.add(addr_range(1,9),100);
+
+	typedef pair < sc_uint<DATA_WIDTH>,sc_uint<DATA_WIDTH> > data_range;
+	scv_bag<data_range> data_list1;
+	data_list1.add(data_range(80,(1<<DATA_WIDTH)-1),100);
+
 
         // Verification I
         cout << "------------------- Beginning of Verification I" << endl;
         for(i=0; i<5; i++){
           // Set writing mode for RAM
-	  // ...
- 
+	  		tbCE.write(0);
+			tbWE.write(0);
           // Generate values for tInData & tAddr using "cPkt"
-	  // ...
-	           
+			cPkt.sAddr->set_mode(addr_list);
+			cPkt.sInData->set_mode(data_list1);
+			cPkt.next();
+			tAddr.write(cPkt.sAddr->read());
+			tInData.write(cPkt.sInData->read());
+			sc_start(1);
           // Set reading mode for RAM
-	  // ...
-
+	  		tbCE.write(0);
+			tbWE.write(1);
+			sc_start(1);
           // Data read from tOutData, save to cPkt.sOutData
-	  // ...
-
+		  	*cPkt.sOutData = tOutData.read();
           // Print statistics
-	  cPkt.print();
-	  sc_start(1);
+	  		cPkt.print();
+	  		sc_start(1);
         }
         cout << "------------------- End of Verification I" << endl << endl;
         
@@ -78,22 +91,28 @@ int sc_main (int argc, char* argv[]) {
         cout << "------------------- Beginning of Verification II" << endl;        
         for(i=0; i<5; i++){
           // Set writing mode for RAM
-	  // ...
-         
+	  		tbCE.write(0);
+			tbWE.write(0);
           // Set range distribution
-	  // ...
-          
+		scv_bag<data_range> data_list2;
+			data_list2.add(data_range(80,99),5);
+			data_list2.add(data_range(100,120),95);
 	  // Generate values of tInData  using "cPkt"
-	  // ...
-          
+			cPkt.sAddr->set_mode(addr_list);
+			cPkt.sInData->set_mode(data_list2);
+			cPkt.next();
+			tAddr.write(cPkt.sAddr->read());
+			tInData.write(cPkt.sInData->read());
+			sc_start(1);
           // Set reading mode for RAM
-	  // ...
-
+	  		tbCE.write(0);
+			tbWE.write(1);
+			sc_start(1);
           // Data read from tOutData, save to cPkt.sOutData
-	  // ...
-
+		  	*cPkt.sOutData = tOutData.read();
           // Print statistics
-	  // ...
+	  		cPkt.print();
+	  		sc_start(1);
         }
         cout << "------------------- End of Verification II" << endl;
         
